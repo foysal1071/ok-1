47c66b382aa537d98dfc440e56571479e734e820
Zahid Hasan Foysal <105988923+foysal1071@users.noreply.github.com>
2022-06-25 14:17:49 +0600

    Create problem 2.c

---
+++ problem 2.c
@@ -0,0 +1,23 @@
+////The distance between Mirpur and Ashulia is input through the keyboard. Write a program to 
+////convert and print this distance in meter, feet, inches and centimeters.
+
+#include<stdio.h>
+int main()
+{
+	float km,meter,feet,inches,centimeter;
+	printf("Enter distance from Mirpur to Ashulia: ");
+	scanf("%f",&km);
+	meter=km*1000; ///1km=1000meters
+	feet=km*3280.8399; ///1km=3280.8399 feets
+	inches=km*39370.078; ///1km=39370.078 inches
+	centimeter=km*100000; ///1km=100000 centimeter
+	
+	printf("Distance in meter from Mirpur to Ashulia:%.2f\n",meter);
+	printf("Distance in feet from Mirpur to Ashulia:%.2f\n",feet);
+	printf("Distance in inches from Mirpur to Ashulia:%.2f\n",inches);
+	printf("Distance in centimeter from Mirpur to Ashulia:%.2f\n",centimeter);
+	
+	
+	
+	return 0;
+}
